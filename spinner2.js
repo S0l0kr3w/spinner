e2dd48eac419a34dfd802fce53afc273161275ca
Jason Junio <jjjunio@gmail.com>
2020-05-05 18:46:21 +0000

    created spinner2.js a refactor of spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const spinner = function(array) {
+for (let char in array)
+setTimeout(() => {
+  process.stdout.write(array[char]);
+}, 100 * char);
+};
+
+const spinnerAnimation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r/  ', '\r-  ', '\r\\  ', '\r|  '] 
+
+spinner(spinnerAnimation);
