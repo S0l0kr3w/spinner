16b0add651f6855a154f638a3fccca92b230de49
Jason Junio <jjjunio@gmail.com>
2020-05-10 13:40:00 +0000

    applied linting on code

--- spinner1.js
+++ spinner1.js
@@ -13,31 +13,26 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r/  '); 
 }, 900);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r-  '); 
 }, 1100);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
+ 
   process.stdout.write('\r\\  '); 
 }, 1300);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|  '); 
 }, 1500);
 
 };
 
 spinner();
-// ... fill in the rest yourself ...
--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,12 @@
 const spinner = function(array) {
-for (let char in array)
-setTimeout(() => {
-  process.stdout.write(array[char]);
-}, 100 * char);
+
+  for (let char in array)
+    setTimeout(() => {
+      process.stdout.write(array[char]);
+    }, 100 * char);
+  
 };
 
-const spinnerAnimation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r/  ', '\r-  ', '\r\\  ', '\r|  '] 
+const spinnerAnimation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r/  ', '\r-  ', '\r\\  ', '\r|  '];
 
 spinner(spinnerAnimation);
